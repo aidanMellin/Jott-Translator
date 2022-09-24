5f8df653443eadbc7ecd6ddda9443e2b8c3216f5
Eli <epm9587@rit.edu>
2022-09-24 18:20:42 -0400

    Stubbed out nodes for characters, numbers, ids, function lists, and programs

---
+++ NumNode.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class NumNode extends JottTree {
+
+    public enum num_type{
+        INT,
+        DOUBLE
+    }
+
+    private num_type type;
+    private String numStored;
+
+    public NumNode(){
+
+    }
+     /**
+     * Will output a string of this tree in Jott
+     * @return a string representing the Jott code of this tree
+     */
+    public String convertToJott()
+    {
+        return("");
+    }
+
+    /**
+     * Will output a string of this tree in Java
+     * @return a string representing the Java code of this tree
+     */
+    public String convertToJava()
+    {
+        return("");
+    }
+
+    /**
+     * Will output a string of this tree in C
+     * @return a string representing the C code of this tree
+     */
+    public String convertToC()
+    {
+        return("");
+    }
+
+    /**
+     * Will output a string of this tree in Python
+     * @return a string representing the Python code of this tree
+     */
+    public String convertToPython()
+    {
+        return("");
+    }
+
+    /**
+     * This will validate that the tree follows the semantic rules of Jott
+	 * Errors validating will be reported to System.err
+     * @return true if valid Jott code; false otherwise
+     */
+    public boolean validateTree()
+    {
+        return(false);
+    }
+}
