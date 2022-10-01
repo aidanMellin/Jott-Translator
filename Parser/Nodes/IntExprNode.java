0ae828210c3e0d0a29906e64e777ef6a5e6a2cb7
Eli <epm9587@rit.edu>
2022-10-01 17:56:58 -0400

    Worked out most of the constructor for IntExprNode, still need cases for another IntExprNode and a func_call

--- Parser/Nodes/IntExprNode.java
+++ Parser/Nodes/IntExprNode.java
@@ -7,9 +7,38 @@ import java.util.ArrayList;
 public class IntExprNode implements JottTree {
     
     private ArrayList<JottTree> subnodes;
-    
+    private ArrayList<Token> Tokens;
+    //Currently missing information needed for handling another IntExprNode & a func_call
     public IntExprNode(ArrayList<Token> tokens) {
-        
+        Tokens = tokens;
+        for (int i = 0; i < Tokens.size(); i++) {
+            Token temp_token = Tokens.get(i);
+            if (temp_token.getTokenType() == TokenType.ID_KEYWORD) {
+                subnodes.add(new IdNode(temp_token));
+            } else if (temp_token.getTokenType() == TokenType.NUMBER) {
+                ArrayList<Token> temp_token_list = new ArrayList<>();
+                temp_token_list.add(temp_token);
+                subnodes.add(new IntNode(temp_token_list));
+
+            } else if (temp_token.getTokenType() == TokenType.MATH_OP) {
+                if (temp_token.getToken().equals("-")) {
+                    if (i == 0 || Tokens.get(i - 1).getTokenType() != TokenType.NUMBER) {
+                        ArrayList<Token> temp_token_list = new ArrayList<>();
+                        temp_token_list.add(temp_token);
+                        if (Tokens.get(i + 1).getTokenType() != TokenType.NUMBER) {
+                            System.out.println("ERROR: Invalid expression, negative sign was not followed by a number.");
+                            return;
+                        } else {
+                            temp_token_list.add(Tokens.get(i + 1));
+                            subnodes.add(new IntNode(temp_token_list));
+                            i++;
+                        }
+                    } else{
+                        subnodes.add(new OpNode(temp_token));
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -18,7 +47,11 @@ public class IntExprNode implements JottTree {
      */
     public String convertToJott()
     {
-        return("");
+        String str = "";
+        for(int i = 0; i < subnodes.size(); i++){
+            str += subnodes.get(i).convertToJott();
+        }
+        return str;
     }
 
     /**
