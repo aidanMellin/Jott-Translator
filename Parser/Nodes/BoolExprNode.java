faa1b2bae5913922f2d0322559363d15c1add610
Eli <epm9587@rit.edu>
2022-10-03 15:34:43 -0400

    only case missing is for if the input is a bool

--- Parser/Nodes/BoolExprNode.java
+++ Parser/Nodes/BoolExprNode.java
@@ -7,8 +7,117 @@ import java.util.ArrayList;
 public class BoolExprNode implements JottTree { //TODO
 
     private ArrayList<JottTree> subnodes;
+    private ArrayList<Token> Tokens;
 
     public BoolExprNode(ArrayList<Token> tokens) {
+        Tokens = tokens;
+        subnodes = new ArrayList<>();
+
+        ArrayList<JottTree> temp_subnodes = new ArrayList<>();
+        ArrayList<Token> tokens_used = new ArrayList<>();
+        int op_count = 0;
+
+        for (int i = 0; i < Tokens.size(); i++) {
+            Token temp_token = Tokens.get(i);
+
+            if(temp_token.getTokenType() == TokenType.NUMBER){
+                int count = i;
+                boolean is_double = temp_token.getToken().contains(".");
+                ArrayList<Token> num_tokens = new ArrayList<>();
+                while(temp_token.getTokenType() != TokenType.REL_OP){
+                    num_tokens.add(temp_token);
+                    count++;
+                    temp_token = Tokens.get(count);
+                }
+                if(is_double){
+                    temp_subnodes.add(new DoubleExprNode(num_tokens));
+                } else{
+                    temp_subnodes.add(new IntExprNode(num_tokens));
+                }
+                tokens_used.addAll(num_tokens);
+                i = count - 1;
+            } else if(temp_token.getTokenType() == TokenType.MATH_OP){
+                Token prev_token = null;
+                if(i > 0){
+                    prev_token = Tokens.get(Tokens.size()-1);
+                }
+                boolean is_double = true;
+                boolean use_prev = true;
+                if(prev_token != null) {
+                    if (prev_token.getTokenType() == TokenType.NUMBER) {
+                        is_double = temp_token.getToken().contains(".");
+                    } else if (prev_token.getTokenType() != TokenType.ID_KEYWORD) {
+                        use_prev = false;
+                        Token next = Tokens.get(i + 1);
+                        if (next.getTokenType() == TokenType.NUMBER) {
+                            is_double = temp_token.getToken().contains(".");
+                        }
+                    }
+                } else{
+                    use_prev = false;
+                    Token next = Tokens.get(i + 1);
+                    if (next.getTokenType() == TokenType.NUMBER) {
+                        is_double = temp_token.getToken().contains(".");
+                    }
+                }
+                int count = i;
+                ArrayList<Token> num_tokens = new ArrayList<>();
+                while(temp_token.getTokenType() != TokenType.REL_OP){
+                    num_tokens.add(temp_token);
+                    count++;
+                    temp_token = Tokens.get(count);
+                }
+                ArrayList<Token> temp = new ArrayList<>();
+                if(use_prev){
+                    temp.add(prev_token);
+                }
+                temp.addAll(num_tokens);
+
+                if(is_double){
+                    temp_subnodes.add(new DoubleExprNode(temp));
+                } else{
+                    temp_subnodes.add(new IntExprNode(temp));
+                }
+                i = count - 1;
+                tokens_used.addAll(num_tokens);
+            }else if(temp_token.getTokenType() == TokenType.REL_OP){
+                temp_subnodes.add(new RelOpNode(temp_token));
+                op_count++;
+                if(op_count > 1){   //once a second rel op has happened, the program takes the tokens used so far and makes them into a seperate node
+                    BoolExprNode temp_condense = new BoolExprNode(tokens_used);
+                    subnodes.add(temp_condense);
+                    subnodes.add(temp_subnodes.get(temp_subnodes.size()-1));
+                    temp_subnodes = new ArrayList<>();
+                    op_count = 0;
+                    tokens_used = new ArrayList<>();
+                }
+                if(op_count == 0) {
+                    tokens_used.add(temp_token);
+                }
+            } else if(temp_token.getTokenType() == TokenType.STRING){
+                temp_subnodes.add(new StrExprNode(temp_token));
+                tokens_used.add(temp_token);
+            } else if(temp_token.getTokenType() == TokenType.ID_KEYWORD){
+                if(i < tokens.size() - 1 && Tokens.get(i+1).getTokenType() == TokenType.L_BRACKET){
+                    int count = 1;
+                    ArrayList<Token> tokens_to_send = new ArrayList<>();
+                    Token current_token = temp_token;
+                    tokens_to_send.add(current_token);
+                    while (count < Tokens.size() && current_token.getTokenType() != TokenType.R_BRACKET){
+                        current_token = Tokens.get(count);
+                        tokens_to_send.add(current_token);
+                        count++;
+                    }
+                    temp_subnodes.add(new FunctionCallNode(tokens_to_send));
+                    i += count;
+                    tokens_used.addAll(tokens_to_send);
+                } else {
+                    temp_subnodes.add(new IdNode(temp_token));
+                    tokens_used.add(temp_token);
+                }
+            }
+
+        }
 
     }
 
@@ -18,7 +127,11 @@ public class BoolExprNode implements JottTree { //TODO
      */
     public String convertToJott()
     {
-        return("");
+        String str = "";
+        for(int i = 0; i < subnodes.size(); i++){
+            str += subnodes.get(i).convertToJott();
+        }
+        return str;
     }
 
     /**
@@ -56,5 +169,10 @@ public class BoolExprNode implements JottTree { //TODO
     public boolean validateTree()
     {
         return(false);
+    }
+
+    @Override
+    public void CreateSyntaxError(String msg, Token token) {
+
     }
 }
