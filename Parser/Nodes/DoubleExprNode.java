1a75238bada06a17f12987d64443a1d5e797b83d
Eli <epm9587@rit.edu>
2022-10-02 21:54:20 -0400

    Used code from IntExprNode with small modifications to make DoubleExprNode

--- Parser/Nodes/DoubleExprNode.java
+++ Parser/Nodes/DoubleExprNode.java
@@ -7,10 +7,74 @@ import java.util.ArrayList;
 public class DoubleExprNode implements JottTree {
 
     private ArrayList<JottTree> subnodes;
+    private ArrayList<Token> Tokens;
 
     public DoubleExprNode(ArrayList<Token> tokens) {
+        Tokens = tokens;
+        subnodes = new ArrayList<>();
 
+        ArrayList<JottTree> temp_subnodes = new ArrayList<>();
+        ArrayList<Token> tokens_used = new ArrayList<>();
+        int op_count = 0;
+
+        for (int i = 0; i < Tokens.size(); i++) {
+            Token temp_token = Tokens.get(i);
+            if (temp_token.getTokenType() == TokenType.ID_KEYWORD) {
+                if(i < tokens.size() - 1 && Tokens.get(i+1).getTokenType() == TokenType.L_BRACKET){
+                    int count = 1;
+                    ArrayList<Token> tokens_to_send = new ArrayList<>();
+                    Token current_token = temp_token;
+                    tokens_to_send.add(current_token);
+                    while (count < Tokens.size() && current_token.getTokenType() != TokenType.R_BRACKET){
+                        current_token = Tokens.get(count);
+                        tokens_to_send.add(current_token);
+                        count++;
+                    }
+                    temp_subnodes.add(new FunctionCallNode(tokens_to_send));
+                    i += count;
+                } else {
+                    temp_subnodes.add(new IdNode(temp_token));
+                }
+            } else if (temp_token.getTokenType() == TokenType.NUMBER) {
+                ArrayList<Token> temp_token_list = new ArrayList<>();
+                temp_token_list.add(temp_token);
+                temp_subnodes.add(new DoubleNode(temp_token_list));
+
+            } else if (temp_token.getTokenType() == TokenType.MATH_OP) {
+                if (temp_token.getToken().equals("-")) {
+                    if (i == 0 || Tokens.get(i - 1).getTokenType() != TokenType.NUMBER) {
+                        ArrayList<Token> temp_token_list = new ArrayList<>();
+                        temp_token_list.add(temp_token);
+                        if (Tokens.get(i + 1).getTokenType() != TokenType.NUMBER) {
+                            System.out.println("ERROR: Invalid expression, negative sign was not followed by a number.");
+                            return;
+                        } else {
+                            temp_token_list.add(Tokens.get(i + 1));
+                            temp_subnodes.add(new DoubleNode(temp_token_list));
+                            i++;
+                        }
+                    } else{
+                        temp_subnodes.add(new OpNode(temp_token));
+                        op_count++;
+                    }
+                } else {
+                    temp_subnodes.add(new OpNode(temp_token));
+                    op_count++;
+                }
+                if(op_count > 1){   //once a second math op has happened, the program takes the tokens used so far and makes them into a seperate node
+                    DoubleExprNode temp_condense = new DoubleExprNode(tokens_used);
+                    subnodes.add(temp_condense);
+                    subnodes.add(temp_subnodes.get(temp_subnodes.size()-1));
+                    temp_subnodes = new ArrayList<>();
+                    op_count = 0;
+                }
+            }
+
+            tokens_used.add(temp_token);
+        }
+        subnodes.addAll(temp_subnodes);
     }
+
 
     /**
      * Will output a string of this tree in Jott
@@ -18,7 +82,11 @@ public class DoubleExprNode implements JottTree {
      */
     public String convertToJott()
     {
-        return("");
+        String str = "";
+        for(int i = 0; i < subnodes.size(); i++){
+            str += subnodes.get(i).convertToJott();
+        }
+        return str;
     }
 
     /**
