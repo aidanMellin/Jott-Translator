d4812f82b5c28df7619fa38fd690130259192ca6
Ethan Iannicelli <etiannicelli@gmail.com>
2022-10-04 13:59:23 -0400

    elseif_lst node

--- Parser/Nodes/ElseIfListNode.java
+++ Parser/Nodes/ElseIfListNode.java
@@ -3,8 +3,9 @@ import Tokenizer.*;
 import Parser.*;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class ElseIfListNode implements JottTree{ //TODO
+public class ElseIfListNode implements JottTree{
 
     private final String JOTT_ELSEIF = "elseif";
     private final String LBRACKET_CHAR = "[";
@@ -17,7 +18,44 @@ public class ElseIfListNode implements JottTree{ //TODO
     private ArrayList<Token> tokens;
 
     public ElseIfListNode(ArrayList<Token> tokens) {
-
+        this.tokens = tokens;
+        if (this.tokens.size() == 0) subnodes = null;
+        else {
+            if (!Objects.equals(this.tokens.get(0).getToken(), JOTT_ELSEIF))
+                CreateSyntaxError("Unexpected Token - Expected 'elseif'", this.tokens.get(0));
+            this.tokens.remove(0);
+            if (this.tokens.get(0).getTokenType() != TokenType.L_BRACKET)
+                CreateSyntaxError("Unexpected Token - Expected '['", this.tokens.get(0));
+            this.tokens.remove(0);
+            ArrayList<Token> b_expr = new ArrayList<>();
+            int b_count = 1;
+            while (b_count != 0) {
+                if (this.tokens.get(0).getTokenType() == TokenType.L_BRACKET) b_count++;
+                b_expr.add(this.tokens.remove(0));
+                if (this.tokens.size() == 0)
+                    CreateSyntaxError("Error: empty token array", b_expr.get(b_expr.size() - 1));
+                if (this.tokens.get(0).getTokenType() == TokenType.R_BRACKET) b_count--;
+            }
+            subnodes.add(new BoolExprNode(b_expr));
+            if (this.tokens.get(0).getTokenType() != TokenType.R_BRACKET)
+                CreateSyntaxError("Unexpected Token - Expected ']'", this.tokens.get(0));
+            this.tokens.remove(0);
+            if (this.tokens.get(0).getTokenType() != TokenType.L_BRACE)
+                CreateSyntaxError("Unexpected Token - Expected '{'", this.tokens.get(0));
+            this.tokens.remove(0);
+            ArrayList<Token> body = new ArrayList<>();
+            b_count = 1;
+            while (b_count != 0) {
+                if (this.tokens.get(0).getTokenType() == TokenType.L_BRACE) b_count++;
+                body.add(this.tokens.remove(0));
+                if (this.tokens.size() == 0) CreateSyntaxError("Error: empty token array", body.get(body.size() - 1));
+                if (this.tokens.get(0).getTokenType() == TokenType.R_BRACE) b_count--;
+            }
+            if (this.tokens.get(0).getTokenType() != TokenType.R_BRACE)
+                CreateSyntaxError("Unexpected Token - Expected '}'", this.tokens.get(0));
+            this.tokens.remove(0);
+            subnodes.add(new ElseIfListNode(this.tokens));
+        }
     }
 
     /**
@@ -26,7 +64,14 @@ public class ElseIfListNode implements JottTree{ //TODO
      */
     public String convertToJott()
     {
-        return("");
+        if (subnodes == null) return EMPTY_STRING;
+        else return JOTT_ELSEIF +
+                LBRACKET_CHAR +
+                subnodes.get(0).convertToJott() +
+                RBRACKET_CHAR +
+                LBRACE_CHAR +
+                subnodes.get(0).convertToJott() +
+                RBRACE_CHAR;
     }
 
     /**
